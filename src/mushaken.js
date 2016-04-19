aff33f9a3c027c2d74780be02233cfd91fd9e28d
fabon <syobon.hinata.public@gmail.com>
2016-04-19 16:50:23 +0900

    Beautify code

--- src/mushaken.js
+++ src/mushaken.js
@@ -1,17 +1,22 @@
 /* eslint-env node,browser */
 /* global createjs */
 (() => {
-    const canvas = document.getElementById("mushaken-main");
+    // const FPS = 60;
     const gameWidth = window.innerWidth;
     const gameHeight = window.innerHeight;
-    // const FPS = 60;
+
+    const canvas = document.getElementById("mushaken-main");
     canvas.width = gameWidth;
     canvas.height =  gameHeight;
+
     const stage = new createjs.Stage("mushaken-main");
+
     const judgeLine = new createjs.Shape();
     judgeLine.graphics.beginFill("black").drawRect(0, 0, 3, gameHeight);
     judgeLine.x = gameWidth / 4;
     judgeLine.y = 0;
+
     stage.addChild(judgeLine);
+
     stage.update();
 }) ();
